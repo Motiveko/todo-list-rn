39a1e79ac56b946d1fa79727b895794774acbb68
motiveko <rhehdrla@naver.com>
2025-05-19 15:27:49 +0900

    feat: todo api 추가

--- apis/index.ts
+++ apis/index.ts
@@ -1,12 +1,14 @@
 import * as user from "@/apis/user";
-
+import * as todo from "@/apis/todo";
 export interface DataAndMessageResponse<T> extends ApiResponse<T> {
   message: string;
 }
+
 export interface ApiResponse<T> {
   data: T;
 }
 
 export const API = {
   user,
+  todo,
 };
---
+++ apis/todo.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+import httpClient from "@/lib/http";
+import { DataAndMessageResponse } from "@/apis";
+
+export interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  isDone: boolean;
+  createdAt: Date;
+}
+export interface CreateTodoDto {
+  title: string;
+  description: string;
+}
+const TodoSchema: z.ZodType<Todo> = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  isDone: z.boolean(),
+  createdAt: z.coerce.date(),
+});
+
+const ListApiResponseSchema: z.ZodType<DataAndMessageResponse<Todo[]>> =
+  z.object({
+    data: z.array(TodoSchema),
+    message: z.string(),
+  });
+
+const CreateApiResponseSchema: z.ZodType<DataAndMessageResponse<Todo>> =
+  z.object({
+    data: TodoSchema,
+    message: z.string(),
+  });
+
+export const list = async () => {
+  const response = await httpClient.get<typeof ListApiResponseSchema>(
+    "/api/v1/todo",
+    {
+      schema: ListApiResponseSchema,
+    }
+  );
+  return response.data;
+};
+
+export const create = async (body: CreateTodoDto) => {
+  const response = await httpClient.post<typeof CreateApiResponseSchema>(
+    "/api/v1/todo",
+    {
+      schema: CreateApiResponseSchema,
+      data: body,
+    }
+  );
+  return response.data;
+};
+
+const _delete = async (id: number) => {
+  await httpClient.delete(`/api/v1/todo/${id}`);
+};
+
+// delete 예약어 충돌 문제로 사용 불가능해서 별도 export 처리
+export { _delete as delete };
